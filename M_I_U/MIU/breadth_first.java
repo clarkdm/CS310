642f38998e2286e13c52776d21ce6ced6483310a
clarkdm <dmclark@hotmail.co.uk>
2015-03-09 01:38:57 +0000

    breadth_first started v2

--- M_I_U/MIU/breadth_first.java
+++ M_I_U/MIU/breadth_first.java
@@ -1,10 +1,37 @@
 package MIU;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class breadth_first {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+
+	private MIU miu;
+	
+	public breadth_first(String target) {
+		String start = "mi";
+		miu = new MIU();
+		
+		
+	}
+	
+	
+	public List<List<String>> extendPath(List<String> start) {
+		List<List<String>> x = new ArrayList<List<String>>();
+		
+for (List<String>  : x) {
+	
+}
+		
+		temp = miu .next_states(start.get(start.size()))
+		
+		
+		return x;
+		
 	}
 
 }
